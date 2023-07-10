db3f1413d441a67428c87b3f8ca8dc3e060c4e9e
showkhin1997 <showkhin15@gmail.com>
2023-07-10 18:01:11 +0600

    object with template string

--- template-string.js
+++ template-string.js
@@ -2,3 +2,17 @@ const phoneNumber = '0185847++++'
 const myInfo = `Hi, I am Showkhin,
 you can call me this ${phoneNumber} number any time!`;
 console.log(myInfo);
+
+
+const phone = {
+    name: 'Redmi K20 Pro',
+    price: 49000,
+    ram: '8GB',
+    rom: '256GB'
+}
+
+const phoneInfo = `
+    My phone brand is ${phone.name} and it's price is ${phone.price}Tk.
+    It have ${phone.ram} ram & also rom is ${phone.rom}.
+`
+console.log(phoneInfo);
