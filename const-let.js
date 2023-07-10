09a3a948e145f568e555232dbe93f17605066994
showkhin1997 <showkhin15@gmail.com>
2023-07-10 17:51:49 +0600

    first commit

---
+++ const-let.js
@@ -0,0 +1,14 @@
+// using const variable
+const firstName = 'Minhajul Islam';
+const secondName = 'Showkhin';
+const fullName = firstName + ' ' + secondName;
+console.log(fullName);
+
+// using let variable
+const array = [1, 3, 4, 6];
+let output = [];
+for (let i = 0; i < array.length; i++) {
+    let result = (array[i]) * 2;
+    output.push(result);
+}
+console.log(output);
